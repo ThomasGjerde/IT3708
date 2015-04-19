d148a753f75dcf70a0305673bb7b4e9488747107
Thomas Gjerde <thomasgjerde90@gmail.com>
2015-04-19 21:39:46 +0200

    Board

--- EA/src/flatland/Board.java
+++ EA/src/flatland/Board.java
@@ -1,13 +1,61 @@
 package flatland;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import model.CellContent;
+import model.Parameters;
 
 public class Board {
-	static CellContent[] staticBoard;
+	static CellContent[][] staticBoard;
 	Agent agent;
-	CellContent cells[];
+	CellContent cells[][];
 	public Board(){
-	Agent agent = new Agent();	
-	
+		Agent agent = new Agent();	
+		if(Parameters.FL_STATIC_BOARD){
+			if(staticBoard == null){
+				generateRandomCells();
+				staticBoard = copyCells(cells);
+			}else{
+				cells = copyCells(staticBoard);
+			}
+		}else{
+			generateRandomCells();
+		}
+	}
+	private void generateRandomCells(){
+		//Check for nullpointers
+		ArrayList<CellContent> cellList = new ArrayList<CellContent>();
+		int totalNumCells = cells.length * cells[0].length;
+		int numFood = (int)(totalNumCells * 0.33);
+		int numPoison = (int)((totalNumCells - numFood) * 0.33);
+		int numEmpty = totalNumCells - (numFood + numPoison);
+		Random r = new Random();
+		for(int i = 0; i < numFood; i++){
+			cellList.add(CellContent.FOOD);
+		}
+		for(int i = 0; i < numPoison; i++){
+			cellList.add(CellContent.POISON);
+		}
+		for(int i = 0; i < numEmpty; i++){
+			cellList.add(CellContent.EMPTY);
+		}
+		for(int i = 0; i < cells.length; i++){
+			for(int j = 0; j < cells[0].length; j++){
+				cells[i][j] = cellList.remove(r.nextInt(cellList.size()));
+			}
+		}
+	}
+	private static CellContent[][] copyCells(CellContent[][] cells){
+		CellContent[][] newCells = new CellContent[cells.length][cells[0].length];
+		for(int i = 0; i < cells.length; i++){
+			for(int j = 0; j < cells[0].length; j++){
+				newCells[i][j] = cells[i][j];
+			}
+		}
+		return newCells;
+	}
+	public Agent getAgent(){
+		return agent;
 	}
 }
